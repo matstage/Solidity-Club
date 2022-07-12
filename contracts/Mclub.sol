ae4492c0a1adb64ab9d022404315e8c393409f52
Mats Tage Axelsson <matstage@gmail.com>
2022-07-12 18:40:47 +0300

    First Version

---
+++ contracts/Mclub.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-v3.0
+
+pragma solidity >=0.8.7 <0.9.0;
+
+contract  mclub() {
+
+
+    function
+
+    function AddAmember() {
+
+    }
+
+    function MemberAddArt() {
+
+    }
+
+    function MemberSellOwnArt() {
+
+    }
+
+    function MemberSellOthersArt() {
+
+    }
+
+
+
+}
