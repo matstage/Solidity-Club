aa3e11f324bf868a2165c2db5240b58ac4713903
Mats Tage Axelsson <matstage@gmail.com>
2022-07-15 11:12:02 +0300

    My first own code!

--- contracts/Mclub.sol
+++ contracts/Mclub.sol
@@ -3,9 +3,44 @@
 pragma solidity >=0.8.7 <0.9.0;
 
 contract  mclub() {
+    mapping(address => string) public memberName;
 
+
+    struct member {
+        address MemberId;
+        uint8 rank;
+        string nickName;
+        enum State { requested, approved, active };
+    }
+
+
+
+    constructor () public {
+        owner = msg.sender; // Only owner can approve members.
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "Only owner can call this function!");
+        _;
+    }
+
+    modifier notOwner() {
+        require(owner != msg.sender, "All other than owner can call this function!");
+        _;
+    }
+
+    modifier
 
     function
+
+
+
+    function MemberRequest(string _nickname) payable notOwner {
+        require(!msg.sender == owner, "Owner already member!");
+        _;
+
+
+    }
 
     function AddAmember() {
 
